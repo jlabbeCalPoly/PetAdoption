65ec14fe0461e2f63b902c72f04510cc8183fcc9
Julian Labbe <jlabbe@calpoly.edu>
2025-02-09 15:47:06 -0800

    ViewModels for each screen, deleted petViewModel

--- app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,49 +29,120 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.zybooks.petadoption.data.Pet
 import com.zybooks.petadoption.data.PetDataSource
 import com.zybooks.petadoption.data.PetGender
 import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
+import kotlinx.serialization.Serializable
+
+sealed class Routes {
+   @Serializable
+   data object List
+
+   @Serializable
+   data class Detail (
+      val petId: Int
+   )
+
+   @Serializable
+   data class Adopt (
+      val petId: Int
+   )
+}
 
 @Composable
-fun PetApp(
-   petViewModel: PetViewModel = viewModel()
-) {
-   Text("To be implemented...")
+fun PetApp() {
+   val navController = rememberNavController()
+
+   NavHost (
+      navController = navController,
+      startDestination = Routes.List
+   ) {
+      composable<Routes.List> {
+         ListScreen(
+            onImageClick = { pet ->
+               navController.navigate(
+                  Routes.Detail(pet.id)
+               )
+            }
+         )
+      }
+      composable<Routes.Detail> { backStackEntry ->
+         val details: Routes.Detail = backStackEntry.toRoute()
+
+         DetailScreen(
+            petId = details.petId,
+            onAdoptClick = {
+               navController.navigate(
+                  Routes.Adopt(details.petId)
+               )
+            },
+            onUpClick = {
+               navController.navigateUp()
+            }
+         )
+      }
+      composable<Routes.Adopt> { backStackEntry ->
+         val adopt: Routes.Adopt = backStackEntry.toRoute()
+         AdoptScreen(
+            petId = adopt.petId,
+            onUpClick = {
+               navController.navigateUp()
+            }
+         )
+      }
+   }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PetAppBar(
    title: String,
-   modifier: Modifier = Modifier
+   modifier: Modifier = Modifier,
+   canNavigateBack: Boolean = false,
+   onUpClick: () -> Unit = { }
 ) {
    TopAppBar(
       title = { Text(title) },
       colors = TopAppBarDefaults.topAppBarColors(
          containerColor = MaterialTheme.colorScheme.primaryContainer
       ),
-      modifier = modifier
+      modifier = modifier,
+      navigationIcon = {
+         if (canNavigateBack) {
+            IconButton(onClick = onUpClick) {
+               Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+            }
+         }
+      }
    )
 }
 
 @Composable
 fun DetailScreen(
-   pet: Pet,
-   onAdoptClick: () -> Unit,
    modifier: Modifier = Modifier,
-   onUpClick: () -> Unit = { }
+   petId: Int,
+   onAdoptClick: () -> Unit,
+   onUpClick: () -> Unit = { },
+   viewModel: DetailViewModel = viewModel()
 ) {
+   val pet = viewModel.getPet(petId)
    val gender = if (pet.gender == PetGender.MALE) "Male" else "Female"
 
    Scaffold(
       topBar = {
          PetAppBar(
             title = "Details",
+            canNavigateBack = true,
+            onUpClick = onUpClick
          )
       }
    ) { innerPadding ->
@@ -117,7 +194,7 @@ fun PreviewDetailScreen() {
    val pet = PetDataSource().loadPets()[0]
    PetAdoptionTheme {
       DetailScreen(
-         pet = pet,
+         petId = pet.id,
          onAdoptClick = { }
       )
    }
@@ -125,9 +202,9 @@ fun PreviewDetailScreen() {
 
 @Composable
 fun ListScreen(
-   petList: List<Pet>,
    onImageClick: (Pet) -> Unit,
-   modifier: Modifier = Modifier
+   modifier: Modifier = Modifier,
+   viewModel: ListViewModel = viewModel()
 ) {
    Scaffold(
       topBar = {
@@ -141,7 +218,7 @@ fun ListScreen(
          contentPadding = PaddingValues(0.dp),
          modifier = modifier.padding(innerPadding)
       ) {
-         items(petList) { pet ->
+         items(viewModel.petList) { pet ->
             Image(
                painter = painterResource(id = pet.imageId),
                contentDescription = "${pet.type} ${pet.gender}",
@@ -160,8 +237,65 @@ fun ListScreen(
 fun PreviewListScreen() {
    PetAdoptionTheme {
       ListScreen(
-         petList = PetDataSource().loadPets(),
          onImageClick = { }
       )
+   }
+}
+
+@Composable
+fun AdoptScreen(
+   petId: Int,
+   modifier: Modifier = Modifier,
+   onUpClick: () -> Unit = { },
+   viewModel: AdoptViewModel = viewModel()
+) {
+   val pet = viewModel.getPet(petId)
+
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Thank You!",
+            canNavigateBack = true,
+            onUpClick = onUpClick
+         )
+      }
+   ) { innerPadding ->
+      Column(
+         modifier = modifier.padding(innerPadding)
+      ) {
+         Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+               painter = painterResource(pet.imageId),
+               contentDescription = pet.name,
+               modifier = modifier.size(150.dp)
+            )
+            Text(
+               text = "Thank you for adopting ${pet.name}!",
+               modifier = modifier.padding(horizontal = 28.dp),
+               textAlign = TextAlign.Center,
+               style = MaterialTheme.typography.headlineLarge,
+            )
+         }
+         Text(
+            text = "Please pick up your new family member during business hours.",
+            modifier = modifier.padding(6.dp),
+         )
+         Button(
+            onClick = { },
+            modifier = modifier.padding(6.dp)
+         ) {
+            Icon(Icons.Default.Share, null)
+            Text("Share", modifier = modifier.padding(start = 8.dp))
+         }
+      }
+   }
+}
+
+@Preview
+@Composable
+fun PreviewAdoptScreen() {
+   val pet = PetDataSource().loadPets()[0]
+   PetAdoptionTheme {
+      AdoptScreen(pet.id)
    }
 }
